18ef73a8d9140a2d6e2137fb8e5765551cfa9d9e
Juan Ferrer <juan.ferrer.videla@gmail.com>
2026-04-08 23:24:30 -0300

    refactor: replace client components with server components

--- components/create-admin-button.tsx
+++ components/create-admin-button.tsx
@@ -1,21 +1,9 @@
-"use client";
-
 import { createAdminAction } from "@/actions";
-import { useTransition } from "react";
 
 export const CreateAdminButton = () => {
-  const [isPending, startTransition] = useTransition();
   return (
-    <button
-      className="px-6 py-4 bg-amber-900"
-      onClick={() => {
-        startTransition(async () => {
-          await createAdminAction();
-        });
-      }}
-      disabled={isPending}
-    >
-      Register Admin
-    </button>
+    <form action={createAdminAction}>
+      <button className="px-6 py-4 bg-amber-900">Register Admin</button>
+    </form>
   );
 };
--- components/delete-admin-button.tsx
+++ components/delete-admin-button.tsx
@@ -1,22 +1,9 @@
-"use client";
-
 import { deleteAdminAction } from "@/actions";
-import { useTransition } from "react";
 
 export const DeleteAdminButton = () => {
-  const [isPending, startTransition] = useTransition();
-
   return (
-    <button
-      className="px-6 py-4 bg-amber-900"
-      onClick={() => {
-        startTransition(async () => {
-          await deleteAdminAction();
-        });
-      }}
-      disabled={isPending}
-    >
-      DELETE ADMIN
-    </button>
+    <form action={deleteAdminAction}>
+      <button className="px-6 py-4 bg-amber-900">DELETE ADMIN</button>
+    </form>
   );
 };
